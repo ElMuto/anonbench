c0a23a892c00aa192fd91d1b7f1e8452f33e5b37
ElMuto <helmut.spengler@tum.de>
2015-03-19 17:05:37 +0100

    implement cleaner approach for login message using /etc/motd

--- scripts/activateLoginMessage.bat
+++ scripts/activateLoginMessage.bat
@@ -10,9 +10,4 @@ pscp %_path%\scripts\%_messageFile% PCluster-fed2:%_remoteHomeDir%/%_heuraklesDi
 pscp %_path%\scripts\%_messageFile% PCluster-fed3:%_remoteHomeDir%/%_heuraklesDir%
 pscp %_path%\scripts\%_messageFile% PCluster-fed4:%_remoteHomeDir%/%_heuraklesDir%
 
-plink PCluster-fed1 sed -i -e 's/^^#cat %_heuraklesDir%\/%_messageFile%/cat %_heuraklesDir%\/%_messageFile%/' %_remoteHomeDir%/.profile
-plink PCluster-fed2 sed -i -e 's/^^#cat %_heuraklesDir%\/%_messageFile%/cat %_heuraklesDir%\/%_messageFile%/' %_remoteHomeDir%/.profile
-plink PCluster-fed3 sed -i -e 's/^^#cat %_heuraklesDir%\/%_messageFile%/cat %_heuraklesDir%\/%_messageFile%/' %_remoteHomeDir%/.profile
-plink PCluster-fed4 sed -i -e 's/^^#cat %_heuraklesDir%\/%_messageFile%/cat %_heuraklesDir%\/%_messageFile%/' %_remoteHomeDir%/.profile
-
 pause
