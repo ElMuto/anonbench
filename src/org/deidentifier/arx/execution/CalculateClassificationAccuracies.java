1330486d791cd4b7461c6bd70318a941132bfaae
ElMuto <helmut.spengler@gmail.com>
2015-10-31 17:02:39 +0100

    support different classifiers

--- src/org/deidentifier/arx/execution/CalculateClassificationAccuracies.java
+++ src/org/deidentifier/arx/execution/CalculateClassificationAccuracies.java
@@ -8,10 +8,18 @@ import java.io.File;
 import java.util.Random;
 
 import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.trees.J48;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 
 public class CalculateClassificationAccuracies {
+	
+	private enum Classifier {
+		J48,
+		RandomForest,
+		NaiveBayes
+	}
 
 	public static void main(String[] args) {
 		
@@ -28,20 +36,40 @@ public class CalculateClassificationAccuracies {
 		
 		for (String workloadAttribute : workloadAttributes) {
 			
-			Evaluation eval = getClassificationAccuracyFor(data, workloadAttribute);
+			Evaluation eval = getClassificationAccuracyFor(data, workloadAttribute, Classifier.RandomForest);
 			
 			System.out.printf("Accuracy for attribute '" + workloadAttribute + "': \t%.4f\n", eval.pctCorrect());
 		}
 	}
 
-	private static Evaluation getClassificationAccuracyFor(Instances data, String attribute) {
+	private static Evaluation getClassificationAccuracyFor(Instances data, String attribute, Classifier classifier) {
 		
 		data.setClassIndex(data.attribute(attribute).index());		
 		Evaluation eval = null;
 		try {
 			eval = new Evaluation(data);
-			J48 tree = new J48();
-			tree.setOptions(new String[] { "-C", "0.25", "-M", "2" });
+			
+			weka.classifiers.Classifier tree = null;
+			switch (classifier) {
+			case J48:
+				J48 j48Tree = new J48();
+				j48Tree.setConfidenceFactor(0.25f);
+				j48Tree.setMinNumObj(2);
+				tree = j48Tree;
+				break;
+			case NaiveBayes:
+				tree = new NaiveBayes();
+				break;
+			case RandomForest:
+				RandomForest rfTree = new RandomForest();
+				rfTree.setNumTrees(100);
+				rfTree.setNumFeatures(0);
+				rfTree.setSeed(1);
+				tree = rfTree;
+				break;
+			default:
+				break;
+			}			
 			eval.crossValidateModel(tree, data, 10, new Random(1));
 		} catch (Exception e) {
 			e.printStackTrace();
