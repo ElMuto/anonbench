62c59f420b8077e9c0b398921d27c83467de3205
ElMuto <helmut.spengler@tum.de>
2015-11-11 18:37:07 +0100

    change script target

--- scripts/2_getResults.bat
+++ scripts/2_getResults.bat
@@ -2,7 +2,7 @@
 
 Set _path=%WORKSPACE_PATH%\anonbench-fork
 
-Set _cluster_node=4
+Set _cluster_node=2
 
 Set _local_results_path=%_path%\results
 pscp PCluster-fed%_cluster_node%:/home/imse/pc-bench-helmut/results/* %_local_results_path%\
