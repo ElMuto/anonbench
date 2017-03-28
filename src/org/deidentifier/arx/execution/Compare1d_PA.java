a20aed0542109ef3cb18a5c4c995ca9dee2b623a
work <work@MAZE>
2017-03-28 23:17:31 +0200

    switch back to ACS13_NUM

--- src/org/deidentifier/arx/execution/Compare1d_PA.java
+++ src/org/deidentifier/arx/execution/Compare1d_PA.java
@@ -49,8 +49,7 @@ public class Compare1d_PA {
 		BenchmarkDatafile datafile = null;
 		String dataFileName = args[0];
 		if ("ACS13".equals(dataFileName)) {
-			datafile = BenchmarkDatafile.ACS13;
-//			datafile = BenchmarkDatafile.ACS13_NUM;
+			datafile = BenchmarkDatafile.ACS13_NUM;
 		} else if ("ATUS".equals(dataFileName)) {
 			datafile = BenchmarkDatafile.ATUS_NUM;
 		} else if ("IHIS".equals(dataFileName)) {
