769212da0505f0ff150824463d830fc48f917a14
work <work@MAZE>
2017-04-02 18:30:59 +0200

    configure and run new experiments

--- scripts/runCompare1d_PA.sh
+++ scripts/runCompare1d_PA.sh
@@ -8,42 +8,17 @@ done
 #run benchmark
 #Usage: java -Xmx5G -jar <jar>
 
-#fed1
-#java -Xmx5G -jar jars/Compare1d_PA.jar ACS13 ld both &
-#java -Xmx5G -jar jars/Compare1d_PA.jar ACS13 lr both
-
-#fed2
-#java -Xmx5G -jar jars/Compare1d_PA.jar ACS13 le both &
-#java -Xmx5G -jar jars/Compare1d_PA.jar ACS13 t  both &
-#java -Xmx5G -jar jars/Compare1d_PA.jar ACS13 d  both
-
 #fed3
-#java -Xmx5G -jar jars/Compare1d_PA.jar IHIS ld both &
-#java -Xmx5G -jar jars/Compare1d_PA.jar IHIS lr both
-
-#fed4
-java -Xmx5G -jar jars/Compare1d_PA.jar IHIS le both &
-java -Xmx5G -jar jars/Compare1d_PA.jar IHIS t  both &
-java -Xmx5G -jar jars/Compare1d_PA.jar IHIS d  both
+#java -Xmx5G -jar jars/Compare1d_PA.jar IHIS ld MS reverse
 
 #fed1
 #java -Xmx5G -jar jars/Compare1d_PA.jar ATUS ld both &
 #java -Xmx5G -jar jars/Compare1d_PA.jar ATUS lr both
 
 #fed2
-#java -Xmx5G -jar jars/Compare1d_PA.jar ATUS le both &
-#java -Xmx5G -jar jars/Compare1d_PA.jar ATUS t  both &
-#java -Xmx5G -jar jars/Compare1d_PA.jar ATUS d  both
-
-#fed4
-#java -Xmx5G -jar jars/Compare1d_PA.jar IHIS ld MS &
-#java -Xmx5G -jar jars/Compare1d_PA.jar IHIS lr MS
+#java -Xmx5G -jar jars/Compare1d_PA.jar ATUS d both &
+#java -Xmx5G -jar jars/Compare1d_PA.jar ATUS t both &
+#java -Xmx5G -jar jars/Compare1d_PA.jar ATUS le both
 
 #fed1
-#java -Xmx5G -jar jars/Compare1d_PA.jar ATUS ld MS reverse &
-
-#fed3
-#java -Xmx5G -jar jars/Compare1d_PA.jar IHIS lr MS reverse
-
-#fed3
-#java -Xmx5G -jar jars/Compare1d_PA.jar IHIS ld ED reverse
+java -Xmx5G -jar jars/Compare1d_PA.jar ATUS ld ED reverse
