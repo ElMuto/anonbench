8a823593a8e116a51050658e3bc60aa4400c9197
ElMuto <helmut.spengler@tum.de>
2017-04-04 17:16:01 +0200

    add test method testTwoTransformationsWithOverallFourECs()

--- test/org/deidentifier/arx/TestWeightedAvg.java
+++ test/org/deidentifier/arx/TestWeightedAvg.java
@@ -13,20 +13,22 @@ import org.deidentifier.arx.criteria.DisclosureRiskCalculator;
 
 public class TestWeightedAvg {
 	
-	static BenchmarkCriterion[]	criteria		= new BenchmarkCriterion[] { BenchmarkCriterion.K_ANONYMITY, BenchmarkCriterion.T_CLOSENESS_ED };
-	static BenchmarkDatafile	datafile		= BenchmarkDatafile.ACS13;
-	static double				suppFactor		= 0.05;
-	static BenchmarkMeasure		measure			= BenchmarkMeasure.ENTROPY;
-	static String				sa				= "Marital status";
-	static PrivacyModel			privacyModel	= new PrivacyModel("t", 5, 0.2);
+	static final BenchmarkCriterion[]	criteria	= new BenchmarkCriterion[] { BenchmarkCriterion.K_ANONYMITY, BenchmarkCriterion.T_CLOSENESS_ED };
+	static final BenchmarkDatafile		datafile	= BenchmarkDatafile.ACS13;
+	static final double					suppFactor	= 0.05;
+	static final BenchmarkMeasure		measure		= BenchmarkMeasure.ENTROPY;
+	static final String					sa			= "Marital status";
+	static final PrivacyModel			privacyModel= new PrivacyModel("t", 5, 0.2);
 	
+	static Data arxData;
+	static DataDefinition numDataDef;
+	static String[] qiS;	
 	static ARXConfiguration config;
 	static ARXAnonymizer anonymizer;
 	static BenchmarkDataset dataset;
-	static ARXResult result;
 	
-    @BeforeClass
-    public static void setUp() {
+    @Before
+    public void setUp() {
 
     	dataset = new BenchmarkDataset(datafile, criteria, sa);
 
@@ -35,8 +37,16 @@ public class TestWeightedAvg {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+        arxData = dataset.getArxData();
+        numDataDef = arxData.getDefinition();
+        qiS = BenchmarkDataset.getQuasiIdentifyingAttributes(datafile);
         anonymizer = new ARXAnonymizer();
         
+    }
+    
+    @After
+    public void cleanUp() {
+    	arxData.getHandle().release();
     }
     
 	/**
@@ -46,25 +56,42 @@ public class TestWeightedAvg {
 	@Test
 	public void testOneTransformationWithTwosECs() {
 		
-		int[] testTrafo = new int[] { 6, 0, 2 };
+		int[] testTrafo = new int[] { 6, 0, 2 }; // has two ECs
+		
+		anonymizeTrafos(testTrafo, testTrafo);
 
-		Data arxData = dataset.getArxData();
-    	DataDefinition numDataDef = arxData.getDefinition();
-    	String[] qiS = BenchmarkDataset.getQuasiIdentifyingAttributes(datafile);
-    	for (int i = 0; i < qiS.length; i++) {
-    		String qi = qiS[i];
-    		numDataDef.setMinimumGeneralization(qi, testTrafo[i]);
-    		numDataDef.setMaximumGeneralization(qi, testTrafo[i]);
-    	}
-        
-        DisclosureRiskCalculator.prepare();
-        try {
-			result = anonymizer.anonymize(arxData, config);
+		assertEquals(0.307627124, DisclosureRiskCalculator.getDelta().getAvg(), 1e-9);
+		
+		
+	}
+
+    
+	/**
+	 * tests solution space with two transformations containing three ECs overall.
+	 */
+	@Test
+	public void testTwoTransformationsWithOverallFourECs() {
+
+		int[] minLevels = new int[] { 6, 0, 2 }; // has two ECs
+		int[] maxLevels = new int[] { 6, 1, 2 }; // has one EC
+
+		anonymizeTrafos(minLevels, maxLevels);
+
+		assertEquals(0.153813561752151, DisclosureRiskCalculator.getDelta().getAvg(), 1e-15);
+	}
+
+	private void anonymizeTrafos(int[] minLevels, int[] maxLevels) {
+		for (int i = 0; i < qiS.length; i++) {
+			String qi = qiS[i];
+			numDataDef.setMinimumGeneralization(qi, minLevels[i]);
+			numDataDef.setMaximumGeneralization(qi, maxLevels[i]);
+		}
+		try {
+			DisclosureRiskCalculator.prepare();
+			anonymizer.anonymize(arxData, config);
+			DisclosureRiskCalculator.done();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-        DisclosureRiskCalculator.done();
-		assertEquals(DisclosureRiskCalculator.getDelta().getAvg(), 0.307627124, 1e-9);
 	}
-
 }
