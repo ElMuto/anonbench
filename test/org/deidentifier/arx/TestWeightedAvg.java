4278305cab239b8deefc93117c945d03a2778f14
ElMuto <helmut.spengler@tum.de>
2017-04-04 15:22:07 +0200

    first test successful

--- test/org/deidentifier/arx/TestWeightedAvg.java
+++ test/org/deidentifier/arx/TestWeightedAvg.java
@@ -2,15 +2,16 @@ package org.deidentifier.arx;
 
 import org.junit.*;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
-import java.util.Set;
 
 import org.deidentifier.arx.BenchmarkDataset.BenchmarkDatafile;
 import org.deidentifier.arx.BenchmarkSetup.BenchmarkCriterion;
 import org.deidentifier.arx.BenchmarkSetup.BenchmarkMeasure;
-import org.deidentifier.arx.DataType.ARXDate;
+import org.deidentifier.arx.criteria.DisclosureRiskCalculator;
 
-public class TestAggregation {
+public class TestWeightedAvg {
 	
 	static BenchmarkCriterion[]	criteria		= new BenchmarkCriterion[] { BenchmarkCriterion.K_ANONYMITY, BenchmarkCriterion.T_CLOSENESS_ED };
 	static BenchmarkDatafile	datafile		= BenchmarkDatafile.ACS13;
@@ -38,8 +39,13 @@ public class TestAggregation {
         
     }
     
+	/**
+	 * tests solution space with one transformation containing two ECs. One with 35,714 entries and avg=0.240240797. the other
+	 * with 33011 entries and avg=0.380531163. This should result in an weighted average of 0.307627124.
+	 */
 	@Test
-	public void test() {
+	public void testOneTransformationWithTwosECs() {
+		
 		int[] testTrafo = new int[] { 6, 0, 2 };
 
 		Data arxData = dataset.getArxData();
@@ -51,12 +57,14 @@ public class TestAggregation {
     		numDataDef.setMaximumGeneralization(qi, testTrafo[i]);
     	}
         
+        DisclosureRiskCalculator.prepare();
         try {
 			result = anonymizer.anonymize(arxData, config);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		assert(result != null);
+        DisclosureRiskCalculator.done();
+		assertEquals(DisclosureRiskCalculator.getDelta().getAvg(), 0.307627124, 1e-9);
 	}
 
 }
