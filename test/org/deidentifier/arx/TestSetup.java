29ca86d386bbf3ccb8ae22b20ffb9d4f74f857c8
ElMuto <helmut.spengler@tum.de>
2017-04-05 18:36:55 +0200

    refactor test classes

---
+++ test/org/deidentifier/arx/TestSetup.java
@@ -0,0 +1,65 @@
+package org.deidentifier.arx;
+
+import java.io.IOException;
+
+import org.deidentifier.arx.BenchmarkDataset.BenchmarkDatafile;
+import org.deidentifier.arx.BenchmarkSetup.BenchmarkCriterion;
+import org.deidentifier.arx.BenchmarkSetup.BenchmarkMeasure;
+import org.deidentifier.arx.criteria.DisclosureRiskCalculator;
+
+public class TestSetup {
+	
+	private Data arxData;
+	private String[] qiS;	
+	private ARXConfiguration config;
+	private ARXAnonymizer anonymizer;
+	private BenchmarkDataset dataset;
+	
+	public TestSetup(BenchmarkCriterion[] criteria, BenchmarkDatafile datafile, double suppFactor,
+			BenchmarkMeasure measure, String sa, PrivacyModel privacyModel) {
+		super();
+		
+	   	dataset = new BenchmarkDataset(datafile, criteria, sa);
+
+        try {
+			config = BenchmarkDriver.getConfiguration(dataset, suppFactor, measure, sa, privacyModel, dataset.getCriteria());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        arxData = dataset.getArxData();
+        qiS = BenchmarkDataset.getQuasiIdentifyingAttributes(datafile);
+        anonymizer = new ARXAnonymizer();
+	}
+
+
+	public ARXResult anonymizeTrafos(int[] minLevels, int[] maxLevels) {
+        DataDefinition dataDef = arxData.getDefinition();
+        
+        ARXResult result = null;
+        
+		for (int i = 0; i < qiS.length; i++) {
+			String qi = qiS[i];
+			dataDef.setMinimumGeneralization(qi, minLevels[i]);
+			dataDef.setMaximumGeneralization(qi, maxLevels[i]);
+		}
+		
+		try {
+			DisclosureRiskCalculator.prepare();
+			
+			result = anonymizer.anonymize(arxData, config);
+			
+			DisclosureRiskCalculator.summarize();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return result; 
+	}
+
+
+	public void cleanUp() {
+
+		arxData.getHandle().release();
+		
+	}
+}
--- test/org/deidentifier/arx/TestWeightedAvg.java
+++ test/org/deidentifier/arx/TestWeightedAvg.java
@@ -4,8 +4,6 @@ import org.junit.*;
 
 import static org.junit.Assert.*;
 
-import java.io.IOException;
-
 import org.deidentifier.arx.BenchmarkDataset.BenchmarkDatafile;
 import org.deidentifier.arx.BenchmarkSetup.BenchmarkCriterion;
 import org.deidentifier.arx.BenchmarkSetup.BenchmarkMeasure;
@@ -13,38 +11,27 @@ import org.deidentifier.arx.criteria.DisclosureRiskCalculator;
 
 public class TestWeightedAvg {
 	
-	static final BenchmarkCriterion[]	criteria	= new BenchmarkCriterion[] { BenchmarkCriterion.K_ANONYMITY, BenchmarkCriterion.T_CLOSENESS_ED };
-	static final BenchmarkDatafile		datafile	= BenchmarkDatafile.ACS13;
-	static final double					suppFactor	= 0.05;
-	static final BenchmarkMeasure		measure		= BenchmarkMeasure.ENTROPY;
-	static final String					sa			= "Marital status";
-	static final PrivacyModel			privacyModel= new PrivacyModel("t", 5, 0.2);
+	final BenchmarkCriterion[]	criteria	= new BenchmarkCriterion[] { BenchmarkCriterion.K_ANONYMITY, BenchmarkCriterion.T_CLOSENESS_ED };
+	final BenchmarkDatafile		datafile	= BenchmarkDatafile.ACS13;
+	final double					suppFactor	= 0.05;
+	final BenchmarkMeasure		measure		= BenchmarkMeasure.ENTROPY;
+	final String					sa			= "Marital status";
+	final PrivacyModel			privacyModel= new PrivacyModel("t", 5, 0.2);
 	
-	static Data arxData;
-	static String[] qiS;	
-	static ARXConfiguration config;
-	static ARXAnonymizer anonymizer;
-	static BenchmarkDataset dataset;
+	private TestSetup testSetup;
 	
     @Before
     public void setUp() {
 
-    	dataset = new BenchmarkDataset(datafile, criteria, sa);
-
-        try {
-			config = BenchmarkDriver.getConfiguration(dataset, suppFactor, measure, sa, privacyModel, dataset.getCriteria());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-        arxData = dataset.getArxData();
-        qiS = BenchmarkDataset.getQuasiIdentifyingAttributes(datafile);
-        anonymizer = new ARXAnonymizer();
+    	testSetup =  new TestSetup(criteria, datafile, suppFactor, measure, sa, privacyModel);
         
     }
     
     @After
     public void cleanUp() {
-    	arxData.getHandle().release();
+    	
+    	testSetup.cleanUp();
+    	
     }
     
 	/**
@@ -56,7 +43,7 @@ public class TestWeightedAvg {
 		
 		int[] testTrafo = new int[] { 6, 0, 2 }; // has two ECs
 		
-		anonymizeTrafos(testTrafo, testTrafo);
+		testSetup.anonymizeTrafos(testTrafo, testTrafo);
 
 		assertEquals(0.307627124, DisclosureRiskCalculator.getDelta().getAvg(), 1e-9);		
 	}
@@ -71,28 +58,8 @@ public class TestWeightedAvg {
 		int[] minLevels = new int[] { 6, 0, 2 }; // has two ECs
 		int[] maxLevels = new int[] { 6, 1, 2 }; // has one EC
 
-		anonymizeTrafos(minLevels, maxLevels);
+		testSetup.anonymizeTrafos(minLevels, maxLevels);
 
 		assertEquals(0.153813561752151, DisclosureRiskCalculator.getDelta().getAvg(), 1e-15);
-	}
-
-	private void anonymizeTrafos(int[] minLevels, int[] maxLevels) {
-        DataDefinition dataDef = arxData.getDefinition();
-        
-		for (int i = 0; i < qiS.length; i++) {
-			String qi = qiS[i];
-			dataDef.setMinimumGeneralization(qi, minLevels[i]);
-			dataDef.setMaximumGeneralization(qi, maxLevels[i]);
-		}
-		
-		try {
-			DisclosureRiskCalculator.prepare();
-			
-			anonymizer.anonymize(arxData, config);
-			
-			DisclosureRiskCalculator.summarize();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
