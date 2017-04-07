b50ca560c783d89e0284d5e467d6b883e0f0c47a
ElMuto <helmut.spengler@tum.de>
2017-04-07 18:01:37 +0200

    testcase for maxdelta

---
+++ test/org/deidentifier/arx/TestMaxDelta.java
@@ -0,0 +1,58 @@
+package org.deidentifier.arx;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.deidentifier.arx.ARXLattice.ARXNode;
+import org.deidentifier.arx.BenchmarkDataset.BenchmarkDatafile;
+import org.deidentifier.arx.BenchmarkSetup.BenchmarkCriterion;
+import org.deidentifier.arx.BenchmarkSetup.BenchmarkMeasure;
+import org.deidentifier.arx.criteria.DisclosureRiskCalculator;
+import org.deidentifier.arx.criteria.DisclosureRiskCalculator.DisclosureRisk;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author spengler
+ * Checks, why Beta-min == 1, Beta-avg == NaN, and Beta-max == 0
+ */
+public class TestMaxDelta {
+
+	
+	final BenchmarkCriterion[]	criteria	= new BenchmarkCriterion[] { BenchmarkCriterion.K_ANONYMITY, BenchmarkCriterion.L_DIVERSITY_DISTINCT };
+	final BenchmarkDatafile		datafile	= BenchmarkDatafile.ACS13;
+	final double				suppFactor	= 0.05;
+	final BenchmarkMeasure		measure		= BenchmarkMeasure.ENTROPY;
+	final String				sa			= "Education";
+	final PrivacyModel			privacyModel= new PrivacyModel("ld", 5, 18d);
+
+	private TestSetup testSetup;
+	
+    @Before
+    public void setUp() {
+
+    	testSetup =  new TestSetup(criteria, datafile, suppFactor, measure, sa, privacyModel);
+        
+    }
+    
+    @After
+    public void cleanUp() {
+
+    	testSetup.cleanUp();
+    	
+    }
+    
+
+	@Test
+	public void testMinMax() {
+		
+		ARXResult result = testSetup.anonymizeTrafos(new int[] { 0, 0, 0 }, new int[] { 6, 1, 2 });
+		result.getOutput(result.getGlobalOptimum(), false);
+		
+		DisclosureRisk delta = DisclosureRiskCalculator.getDelta();
+		
+		assertTrue("delta-max = " + delta.getMax(), delta.getMax() <= 9.4);
+	}
+}
