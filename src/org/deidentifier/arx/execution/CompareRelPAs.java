6fabd023b32a19e370065c2eeefe39d200999250
work <work@MAZE>
2017-04-11 21:16:18 +0200

    cleanup copyright info and comments

--- src/org/deidentifier/arx/execution/CompareRelPAs.java
+++ src/org/deidentifier/arx/execution/CompareRelPAs.java
@@ -1,23 +1,3 @@
-/*
- * Source code of our CBMS 2014 paper "A benchmark of globally-optimal 
- *      methods for the de-identification of biomedical data"
- *      
- * Copyright (C) 2014 Florian Kohlmayer, Fabian Prasser
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
-
 package org.deidentifier.arx.execution;
 
 import java.io.IOException;
@@ -28,11 +8,6 @@ import org.deidentifier.arx.BenchmarkDataset.BenchmarkDatafile;
 import org.deidentifier.arx.BenchmarkSetup.BenchmarkMeasure;
 
 
-/**
- * Main benchmark class. Run with java -Xmx4G -XX:+UseConcMarkSweepGC -jar pc-bench.jar
- * 
- * @author Helmut Spengler
- */
 public class CompareRelPAs {
 	
 	/**
