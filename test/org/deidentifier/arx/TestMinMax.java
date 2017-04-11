58626fcca8784b357332fe7bb14379a679084efb
work <work@MAZE>
2017-04-11 16:54:02 +0200

    merging

---
+++ test/org/deidentifier/arx/TestMinMax.java
@@ -0,0 +1,55 @@
+package org.deidentifier.arx;
+
+import static org.junit.Assert.*;
+
+import org.deidentifier.arx.BenchmarkDataset.BenchmarkDatafile;
+import org.deidentifier.arx.BenchmarkSetup.BenchmarkCriterion;
+import org.deidentifier.arx.BenchmarkSetup.BenchmarkMeasure;
+import org.deidentifier.arx.criteria.DisclosureRiskCalculator;
+import org.deidentifier.arx.criteria.DisclosureRiskCalculator.DisclosureRisk;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author spengler
+ * Checks, why Beta-min == 1, Beta-avg == NaN, and Beta-max == 0
+ */
+public class TestMinMax {
+
+	
+	final BenchmarkCriterion[]	criteria	= new BenchmarkCriterion[] { BenchmarkCriterion.K_ANONYMITY, BenchmarkCriterion.L_DIVERSITY_DISTINCT };
+	final BenchmarkDatafile		datafile	= BenchmarkDatafile.ACS13;
+	final double				suppFactor	= 0.05;
+	final BenchmarkMeasure		measure		= BenchmarkMeasure.ENTROPY;
+	final String				sa			= "Education";
+	final PrivacyModel			privacyModel= new PrivacyModel("ld", 5, 18d);
+
+	private Setup testSetup;
+	
+    @Before
+    public void setUp() {
+
+    	testSetup =  new Setup(criteria, privacyModel, datafile, suppFactor, measure, sa);
+        
+    }
+    
+    @After
+    public void cleanUp() {
+
+    	testSetup.cleanUp();
+    	
+    }
+    
+
+	@Test
+	public void testDelta() {
+		
+		ARXResult result = testSetup.anonymizeTrafos(new int[] { 0, 0, 0 }, new int[] { 6, 1, 2 });
+		result.getOutput(result.getGlobalOptimum(), false);
+		
+		DisclosureRisk delta = DisclosureRiskCalculator.getDelta();
+		
+		assertTrue("delta-max = " + delta.getMax(), delta.getMax() <= 9.99708812);
+	}
+}
---
+++ test/org/deidentifier/arx/TestNPE_ACS13_lr.java
@@ -0,0 +1,52 @@
+package org.deidentifier.arx;
+
+import org.deidentifier.arx.BenchmarkDataset.BenchmarkDatafile;
+import org.deidentifier.arx.BenchmarkSetup.BenchmarkCriterion;
+import org.deidentifier.arx.BenchmarkSetup.BenchmarkMeasure;
+import org.deidentifier.arx.criteria.DisclosureRiskCalculator;
+import org.deidentifier.arx.criteria.DisclosureRiskCalculator.DisclosureRisk;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Helmut Spengler
+ */
+public class TestNPE_ACS13_lr {
+
+	
+	final BenchmarkCriterion[]	criteria	= new BenchmarkCriterion[] { BenchmarkCriterion.K_ANONYMITY, BenchmarkCriterion.L_DIVERSITY_RECURSIVE };
+	final BenchmarkDatafile		datafile	= BenchmarkDatafile.ACS13;
+	final double				suppFactor	= 0.05;
+	final BenchmarkMeasure		measure		= BenchmarkMeasure.ENTROPY;
+	final String				sa			= "Marital status";
+	final PrivacyModel			privacyModel= new PrivacyModel("lr", 5, 5d);
+
+	private Setup testSetup;
+	
+    @Before
+    public void setUp() {
+
+    	testSetup =  new Setup(criteria, privacyModel, datafile, suppFactor, measure, sa);
+        
+    }
+    
+    @After
+    public void cleanUp() {
+
+    	testSetup.cleanUp();
+    	
+    }
+    
+
+	@Test
+	public void testBigestFoundBeta() {
+		
+		ARXResult result = testSetup.anonymizeTrafos(new int[] { 0, 0, 0 }, new int[] { 6, 1, 2 });
+		result.getOutput(result.getGlobalOptimum(), false);
+		
+		DisclosureRisk beta = DisclosureRiskCalculator.getBeta();
+		
+		assert((beta.getMin() == Double.MAX_VALUE && beta.getMax() == Double.MIN_VALUE) || beta.getMin() <= beta.getMax());
+	}
+}
