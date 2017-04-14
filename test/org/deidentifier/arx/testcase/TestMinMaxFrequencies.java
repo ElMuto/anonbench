6fa758d27a73dad46dc69f42a22a9fde4275d573
work <work@MAZE>
2017-04-14 10:19:57 +0200

    new test

---
+++ test/org/deidentifier/arx/testcase/TestMinMaxFrequencies.java
@@ -0,0 +1,14 @@
+package org.deidentifier.arx.testcase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMinMaxFrequencies {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
