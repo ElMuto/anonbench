edc0ff8c6b6c81888ef0ed87a58f3d3d0a6e15eb
ElMuto <helmut.spengler@tum.de>
2017-05-05 15:51:33 +0200

    parametrize git project

--- Docker/startup.sh
+++ Docker/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cd ${CODE_ADCC}
-git pull
+git clone --verbose --branch ${GIT_BRANCH} ${GIT_URL} ${PROJECT_NAME}
+cd ${PROJECT_NAME}
+mv ../data .
+mv ../hierarchies .
+
 ant -DCUSTOM_TEST_CLASS="${CUSTOM_TEST_CLASS}" ${ANT_TARGET}
