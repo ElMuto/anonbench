b6d3b6f9edd612770b552fc09c1955e634bff720
ElMuto <helmut.spengler@tum.de>
2017-10-24 14:58:06 +0200

    configure benchmark exec

---
+++ scripts/runCompareRuntimes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#deactivate speedstep
+for f in `ls /sys/devices/system/cpu/ | grep cpu[0-9]`; do
+test -e /sys/devices/system/cpu/$f/cpufreq/scaling_governor && echo performance | sudo tee /sys/devices/system/cpu/$f/cpufreq/scaling_governor
+done
+
+#run benchmark
+#Usage: java -Xmx5G -jar <jar>
+
+java -Xmx4G -DHelmut -jar jars/CompareRuntimes.jar
